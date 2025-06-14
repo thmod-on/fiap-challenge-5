ad648645c51ba25026a7fbded7fab67403da6e76
Thiago de Mendonça <thiagodemendonca@gmail.com>
2025-06-14 10:50:00 -0300

    Tratamento inicial dos dados
    Adicionado metodo para ler o json com lista de elementos;
    Adicionado tratamento inicial dos dados, removendo colunas e criando novas

--- main.py
+++ main.py
@@ -1,16 +1,82 @@
+#########################################################################################
+# IMPORTS
+#########################################################################################
+
 import metodos as m
 import pandas as pd
+
+#########################################################################################
+# CARGA DOS DADOS
+#########################################################################################
 
 path_applicants = 'dados\\applicants.zip'
 path_vagas = 'dados\\vagas.zip'
 path_prospects = 'dados\\prospects.zip'
 
 df_applicants = m.ler_json(path_applicants)
-df_vagas = m.ler_json(path_vagas)
-df_prospects = m.ler_json(path_prospects)
+df_vagas      = m.ler_json(path_vagas)
+df_prospects  = m.ler_json_candidaturas("/content/prospects.zip")
 
-print(df_applicants.head())
-print(df_vagas.head())
+#########################################################################################
+# TRATAMENTO DOS DADOS
+#########################################################################################
+#   PROSPECTS
+#########################################################################################
 
-# Precisa de ajuste na leitura deste json
+# Removendo colunas que consideramos nao influenciar na analise
+df_prospects.drop(columns=['nome', 'data_candidatura',
+                           'recrutador', 'ultima_atualizacao',
+                           'comentario', 'recrutador'], inplace=True)
+
 print(df_prospects.head())
+
+#########################################################################################
+#   VAGAS
+#########################################################################################
+
+# Removendo colunas que consideramos nao influenciar na analise
+df_vagas.drop(columns=['informacoes_basicas.data_requicisao', 'informacoes_basicas.limite_esperado_para_contratacao',
+                       'informacoes_basicas.cliente', 'informacoes_basicas.solicitante_cliente',
+                       'informacoes_basicas.empresa_divisao', 'informacoes_basicas.requisitante',
+                       'informacoes_basicas.analista_responsavel','informacoes_basicas.superior_imediato',
+                       'informacoes_basicas.telefone', 'perfil_vaga.demais_observacoes',
+                       'informacoes_basicas.data_inicial', 'informacoes_basicas.data_final',
+                       'informacoes_basicas.nome_substituto','informacoes_basicas.nome',
+                       'informacoes_basicas.origem_vaga', 'perfil_vaga.principais_atividades',
+                       'beneficios.valor_compra_1', 'beneficios.valor_compra_2',
+                       'perfil_vaga.habilidades_comportamentais_necessarias', 'beneficios.valor_venda',
+                       'perfil_vaga.competencia_tecnicas_e_comportamentais', 'perfil_vaga.horario_trabalho'
+                       ], inplace=True)
+
+# Preenchendo nan com o "padrao" das ja utilizado em outras linhas e padronizando demais casos
+df_vagas['perfil_vaga.equipamentos_necessarios'] = df_vagas['perfil_vaga.equipamentos_necessarios'].fillna('Nenhum -')
+df_vagas['perfil_vaga.equipamentos_necessarios'] = df_vagas['perfil_vaga.equipamentos_necessarios'].str \
+  .replace('Outro - Nenhum -', 'Nenhum -') \
+  .replace('Outro -', 'Nenhum -') \
+  .replace('', 'Nenhum -')
+  
+# Extrair os valores numéricos usando regex para ter a faixa etaria de forma mais clara
+# Proposta de uso, no cadastro do candidato podemos realizar um filtro da idade dele pela idade ofertada pelas vagas?
+df_vagas[['idade_minima', 'idade_maxima']] = df_vagas['perfil_vaga.faixa_etaria'].str.extract(r'De:\s*(\d+)\s*Até:\s*(\d+)')
+df_vagas['idade_minima'] = df_vagas['idade_minima'].fillna(18).astype(int)
+df_vagas['idade_maxima'] = df_vagas['idade_maxima'].fillna(60).astype(int)
+
+#########################################################################################
+#   APPLICANTS
+#########################################################################################
+
+# Removendo colunas que consideramos nao influenciar na analise
+df_applicants.drop(columns=['infos_basicas.telefone_recado', 'infos_basicas.telefone',
+                            'infos_basicas.inserido_por', 'infos_basicas.email',
+                            'infos_basicas.sabendo_de_nos_por', 'infos_basicas.nome',
+                            'informacoes_pessoais.nome', 'informacoes_pessoais.cpf',
+                            'informacoes_pessoais.fonte_indicacao', 'informacoes_pessoais.email',
+                            'informacoes_pessoais.email_secundario', 'informacoes_pessoais.data_nascimento',
+                            'informacoes_pessoais.telefone_celular',
+                            'informacoes_pessoais.telefone_recado', 'informacoes_pessoais.sexo',
+                            'informacoes_pessoais.estado_civil', 'informacoes_pessoais.pcd',
+                            'informacoes_pessoais.endereco', 'informacoes_pessoais.skype',
+                            'informacoes_pessoais.url_linkedin', 'informacoes_pessoais.facebook',
+                            'formacao_e_idiomas.instituicao_ensino_superior', 'informacoes_pessoais.download_cv',
+                            'cargo_atual.nome_superior_imediato','cargo_atual.email_superior_imediato',
+                            'cv_pt', 'cv_en', 'formacao_e_idiomas.outro_curso'], inplace=True)
--- metodos.py
+++ metodos.py
@@ -3,24 +3,80 @@ import json
 import pandas as pd
 
 def ler_json(caminho_zip):
-  with zipfile.ZipFile(caminho_zip, 'r') as zip_ref:
-    nome_arquivo_json = zip_ref.namelist()[0]  # assumindo que só há um arquivo no zip
-    with zip_ref.open(nome_arquivo_json) as file:
-        dados = json.load(file)
+    """Metodo criado para ler um arquivo .json que esteja dentro de um .zip
+    Estrutura do json deve ser chave -> elemento - > valor 
+    ou 
+    chave -> categoria -> elemento -> valor
 
-  # Achatar os dados
-  linhas = []
-  for chave, conteudo in dados.items():
-      linha = {"ID": chave}
-      # Flatten (achatar) cada subnível com prefixo
-      for secao, valores in conteudo.items():
-          if isinstance(valores, dict):
-              for subchave, subvalor in valores.items():
-                  linha[f"{secao}.{subchave}"] = subvalor
-          else:
-              linha[secao] = valores
-      linhas.append(linha)
+    Args:
+        caminho_zip (string): caminho onde está o arquivo .zip
 
-  # Criar o DataFrame
-  df = pd.DataFrame(linhas)
-  return df
+    Returns:
+        dataframe: o json sera achatado para retornar um dataframe correspondente, 
+        concatenando a categoria.elemento quando necessário
+    """
+    with zipfile.ZipFile(caminho_zip, 'r') as zip_ref:
+        nome_arquivo_json = zip_ref.namelist()[0]  # assumindo que só há um arquivo no zip
+        with zip_ref.open(nome_arquivo_json) as file:
+            dados = json.load(file)
+
+    # Achatar os dados
+    linhas = []
+    for chave, conteudo in dados.items():
+        linha = {"ID": chave}
+        # Flatten (achatar) cada subnível com prefixo
+        for secao, valores in conteudo.items():
+            if isinstance(valores, dict):
+                for subchave, subvalor in valores.items():
+                    linha[f"{secao}.{subchave}"] = subvalor
+            else:
+                linha[secao] = valores
+        linhas.append(linha)
+
+    # Criar o DataFrame
+    df = pd.DataFrame(linhas)
+    return df
+
+
+def ler_json_candidaturas(caminho_zip):
+    """Similar ao metodo ler_json, porem, contemplando uma lista de elementos dentro 
+    de uma categoria
+
+    Args:
+        caminho_zip (string): caminho onde está o arquivo .zip
+
+    Returns:
+        dataframe: o json sera achatado para retornar um dataframe correspondente, 
+        concatenando a categoria.elemento quando necessário.
+        Para o caso de uma lista de elementos, cada um vai gerar uma nova linha para a
+        mesma chave inicial
+    """
+    with zipfile.ZipFile(caminho_zip, 'r') as zip_ref:
+        nome_arquivo_json = zip_ref.namelist()[0]  # assumindo que só há um arquivo no zip
+        with zip_ref.open(nome_arquivo_json) as file:
+            dados_candidatura = json.load(file)
+
+    linhas_candidatura = []
+
+    for codigo_vaga, conteudo in dados_candidatura.items():
+        prospects = conteudo.get("prospects")
+
+        # Verifica se há prospects e se é uma lista com itens
+        if isinstance(prospects, list) and prospects:
+            for prospect in prospects:
+                linha_candidatura = {
+                    'codigo_vaga': codigo_vaga,
+                    'titulo_vaga': conteudo.get('titulo', ''),
+                    'nome': prospect.get('nome', ''),
+                    'codigo_candidato': prospect.get('codigo', ''),
+                    'situacao_candidato': prospect.get('situacao_candidado', ''),
+                    'data_candidatura': prospect.get('data_candidatura', ''),
+                    'ultima_atualizacao': prospect.get('ultima_atualizacao', ''),
+                    'comentario': prospect.get('comentario', ''),
+                    'recrutador': prospect.get('recrutador', '')
+                }
+                linhas_candidatura.append(linha_candidatura)
+
+    # Criar o DataFrame final
+    df_candidaturas = pd.DataFrame(linhas_candidatura)
+    return df_candidaturas
