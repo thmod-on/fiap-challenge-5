b87309eb7d9186fdaca0645d9bce84dc35395d82
Thiago de Mendonça <thiagodemendonca@gmail.com>
2025-06-27 16:54:13 -0300

    Ajustes nos dados a serem analisados
    Revisadas colunas a serem utilizadas para candidatos e vagas;
    Tratamento de valores ausentes mas possivelmente uteis;
    Início do merge e preparação para treinamento

--- main.py
+++ main.py
@@ -26,27 +26,21 @@ df_prospects  = m.ler_json_candidaturas(path_prospects)
 # Removendo colunas que consideramos nao influenciar na analise
 df_prospects.drop(columns=['nome', 'data_candidatura',
                            'recrutador', 'ultima_atualizacao',
-                           'comentario', 'recrutador'], inplace=True)
+                           'comentario', 'recrutador', 'titulo_vaga'
+                           ], inplace=True)
 
 #########################################################################################
 #   VAGAS
 #########################################################################################
 
 # Removendo colunas que consideramos nao influenciar na analise
-df_vagas = m.remover_colunas(df_vagas, 
-                             ['informacoes_basicas.titulo_vaga', 'informacoes_basicas.cliente'], 
-                             'informacoes_basicas\.')
-
-# df_vagas.drop(columns=[
-#                        'informacoes_basicas.telefone', 'perfil_vaga.demais_observacoes',
-#                        'perfil_vaga.principais_atividades',
-#                        'perfil_vaga.regiao',
-#                        'beneficios.valor_compra_1', 'beneficios.valor_compra_2',
-#                        'perfil_vaga.habilidades_comportamentais_necessarias', 'beneficios.valor_venda',
-#                        'perfil_vaga.competencia_tecnicas_e_comportamentais', 'perfil_vaga.horario_trabalho',
-#                        'perfil_vaga.local_trabalho', 'perfil_vaga.bairro', 'perfil_vaga.faixa_etaria',
-#                        'perfil_vaga.pais', 'perfil_vaga.estado', 'perfil_vaga.cidade'
-#                        ], inplace=True)
+col_vagas_manter = ['informacoes_basicas.titulo_vaga',
+                    'perfil_vaga.vaga_especifica_para_pcd', 'perfil_vaga.nivel profissional',
+                    'perfil_vaga.nivel_academico', 'perfil_vaga.nivel_ingles', 'perfil_vaga.nivel_espanhol',
+                    'perfil_vaga.outro_idioma']
+df_vagas = m.remover_colunas(df_vagas,
+                           col_vagas_manter,
+                           'informacoes_basicas\.|perfil_vaga\.|beneficios\.')
 
 # Preenchendo nan com o "padrao" das ja utilizado em outras linhas e padronizando demais casos
 df_vagas['perfil_vaga.equipamentos_necessarios'] = df_vagas['perfil_vaga.equipamentos_necessarios'].fillna('Nenhum -')
@@ -54,7 +48,9 @@ df_vagas['perfil_vaga.equipamentos_necessarios'] = df_vagas['perfil_vaga.equipam
   .replace('Outro - Nenhum -', 'Nenhum -') \
   .replace('Outro -', 'Nenhum -') \
   .replace('', 'Nenhum -')
-  
+df_vagas['perfil_vaga.vaga_especifica_para_pcd'] = df_vagas['perfil_vaga.vaga_especifica_para_pcd'].replace('', 'Não')
+df_vagas['perfil_vaga.nivel_espanhol'] = df_vagas['perfil_vaga.nivel_espanhol'].replace('', 'Nenhum')
+df_vagas['perfil_vaga.outro_idioma'] = df_vagas['perfil_vaga.outro_idioma'].replace('', 'Nenhum').str.replace(' - ',' ')  
   
 print('\n\ncolunas: ', df_vagas.columns)
 print('\n\nvagas: ', df_vagas.shape)
@@ -64,16 +60,36 @@ print('\n\nvagas: ', df_vagas.shape)
 #########################################################################################
 
 # Removendo colunas que consideramos nao influenciar na analise
-df_applicants = m.remover_colunas(df_applicants, [], 'infos_basicas\.|informacoes_pessoais\.|cargo_atual\.')
+col_applicants_manter = ['informacoes_pessoais.pcd', 'informacoes_profissionais.titulo_profissional',
+                         'informacoes_profissionais.area_atuacao', 'informacoes_profissionais.nivel_profissional']
+df_applicants = m.remover_colunas(df_applicants,
+                                col_applicants_manter,
+                                'infos_basicas\.|informacoes_pessoais\.|informacoes_profissionais\.|cargo_atual\.')
 
 df_applicants.drop(columns=['cv_pt', 'cv_en',
-                            'informacoes_profissionais.remuneracao','informacoes_profissionais.area_atuacao',
-                            'informacoes_profissionais.outras_certificacoes','informacoes_profissionais.experiencias',
                             'formacao_e_idiomas.ano_conclusao', 'formacao_e_idiomas.outro_curso'
                              ], inplace=True)
 
-print('candidatos: ', df_applicants.shape)
+df_applicants['informacoes_pessoais.pcd'] = df_applicants['informacoes_pessoais.pcd'].replace('', 'Não')
+df_applicants['formacao_e_idiomas.outro_idioma'] = df_applicants['formacao_e_idiomas.outro_idioma']. \
+  replace('-', 'Nenhum'). \
+  replace('Português -', 'Nenhum'). \
+  replace('Alemão - Nenhum', 'Nenhum'). \
+  replace('Japonês - Nenhum	', 'Nenhum'). \
+  str.replace(' -', '')
+df_applicants['formacao_e_idiomas.nivel_ingles'] = df_applicants['formacao_e_idiomas.nivel_ingles'].replace('', 'Nenhum')
+df_applicants['formacao_e_idiomas.nivel_espanhol'] = df_applicants['formacao_e_idiomas.nivel_espanhol'].replace('', 'Nenhum')
+df_applicants['formacao_e_idiomas.nivel_academico'] = df_applicants['formacao_e_idiomas.nivel_academico'].replace('', 'Não informado')
+df_applicants['informacoes_profissionais.titulo_profissional'] = df_applicants['informacoes_profissionais.titulo_profissional'].replace('', 'Não informado')
 
 #m.exibir_head_df('CANDIDATOS', df_applicants)
 #m.exibir_head_df('VAGAS', df_vagas)
 #m.exibir_head_df('CANDIDATURA', df_prospects)
+
+#########################################################################################
+#   TREINANDO O MODELO
+#########################################################################################
+
+df_merge = pd. \
+  merge(df_prospects, df_vagas, left_on='codigo_vaga', right_on='ID', how='inner'). \
+  merge(df_applicants, left_on='codigo_candidato', right_on='ID', how='inner')
